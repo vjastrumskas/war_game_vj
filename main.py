4c01f1d4a27b35ad1aacf8dbf1cefd828016ce26
Vaidotas Jastrumskas <vaidotas.jastrumskas@gmail.com>
2023-12-13 20:02:59 +0200

    This is my further commit.

---
+++ main.py
@@ -0,0 +1,24 @@
+
+class Card:
+    def __init__(self, value, suit) -> None:
+        self._value = value
+        self._suit = suit
+
+    @property
+    def get_value(self): 
+        return self._value
+    
+    @age.setter
+    def set_age(self, value): 
+        self._value = value 
+
+    @property
+    def get_value(self): 
+        return self._value
+    
+    @age.setter
+    def set_age(self, value): 
+        self._value = value 
+
+    def __str__(self) -> str:
+        pass
